fd874bd2d515b524d717e8992140a84080c7135b
Jazib Hassan <jazibulhassan95@gmail.com>
2022-12-04 02:08:03 +0000

    Create S3 bucket

--- lib/storage-stack.ts
+++ lib/storage-stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_s3 as s3 } from 'aws-cdk-lib/aws_s3';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class StorageStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'fast-zakhira-storage');
+    new Bucket(this, 'fast-zakhira-storage',{
+    });
   }
 }
