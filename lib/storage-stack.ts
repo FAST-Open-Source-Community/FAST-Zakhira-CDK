6bca4e70b77b144d1d37d19333fd6da20f57e4fc
Jazib Hassan <jazibulhassan95@gmail.com>
2022-12-06 01:22:51 +0000

    Create DynamoDB

--- lib/storage-stack.ts
+++ lib/storage-stack.ts
@@ -1,11 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class StorageStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'fast-zakhira-storage',{
+    const s3_storage = new Bucket(this, 'fast-zakhira-storage', {
+      bucketName: 'fast-zakhira-storage',
+      
+    });
+
+    const fast_zakhira_db = new Table(this, 'fast-zakhira-dynamodb', {
+      partitionKey: { name: 'partitionKey', type: AttributeType.STRING },
+      sortKey : {name: 'sortKey', type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      tableName: 'Fast-Zakhira-Main'
     });
   }
 }
